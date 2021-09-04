45092a07d004c57108b3218cccc68db38cab8350
Chen Caidy <chen@caidy.cc>
2021-09-05 00:31:25 +0800

    minor: change output name

--- image/mesongx.mk
+++ image/mesongx.mk
@@ -10,8 +10,7 @@ define Device/phicomm-n1
   DEVICE_MODEL := N1
   SOC := meson-gxl-s905d
   UBOOT_DEVICE_NAME := phicomm-n1
-  IMAGE/factory.img.gz := boot-common | auto-script phicomm-n1 | boot-script | boot-img | gzip
-  IMAGE/sysupgrade.img.gz := boot-common | auto-script phicomm-n1 | boot-script | boot-img | gzip | append-metadata
+  IMAGE/sdcard.img.gz := boot-common | auto-script phicomm-n1 | boot-script | boot-img | gzip | append-metadata
   DEVICE_PACKAGES := \
     kmod-brcmfmac wpad-basic-wolfssl cypress-firmware-43455-sdio
 endef
--- image/mesonsm1.mk
+++ image/mesonsm1.mk
@@ -10,8 +10,7 @@ define Device/x96-air
   DEVICE_MODEL := X96 Air
   SOC := meson-sm1
   UBOOT_DEVICE_NAME := amedia-x96
-  IMAGE/factory.img.gz := boot-common | boot-script | boot-img | boot-sm1 | gzip
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | boot-img | boot-sm1 | gzip | append-metadata
+  IMAGE/sdcard.img.gz := boot-common | boot-script | boot-img | boot-sm1 | gzip | append-metadata
   DEVICE_PACKAGES := \
     kmod-brcmfmac wpad-basic-wolfssl cypress-firmware-43455-sdio \
     triggerhappy
